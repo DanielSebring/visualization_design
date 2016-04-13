5568a1e457ad4e2d2e05411ccd0bb8276b7d882b
DanielSebring <sebring.d@gmail.com>
2016-04-13 16:00:59 -0700

    all visualizations working correctly time to finish up writing the explanations

--- plotly.js
+++ plotly.js
@@ -3,21 +3,99 @@
 var bacteria_data = [];
             
 d3.csv("data/antibiotics_data.csv", function(error, data) {
+    var bacteriaDataArray = getBacteria(data, "scatter");
+    var bacteria_data = bacteriaDataArray[0];
+    var drugs = [bacteriaDataArray[1], bacteriaDataArray[2], bacteriaDataArray[3]];
+    var nameArray = bacteriaDataArray[4]
 
-    console.log("Here's the data\n" + data);
-   
-    drawFirstVis(data);
+    drawFirstVis(drugs, nameArray);
     drawSecondVis(data);
+    drawThirdVis(bacteria_data, nameArray);
 });
 
-
-
-function drawFirstVis(data) {
+function getBacteria(data, type) {
+    var Penicilin = [];
+    var Streptomycin = [];
+    var Neomycin = [];
+    var nameArray = [];
+    var chartData = [];
     
-    /*
-    Object {"Bacteria ": "Streptococcus fecalis ", Penicilin: "1", "Streptomycin ": "1", Neomycin: "0.1", "Gram Staining ": "positive"}
-    */
+    for( var i = 0; i < data.length; i++) {
+        //console.log(data[i]);
+        nameArray.push(data[i]["Bacteria "]);
+        Penicilin.push(data[i].Penicilin);
+        Streptomycin.push(data[i]["Streptomycin "]);
+        Neomycin.push(data[i].Neomycin);
+        
+        var newChartData = {
+            Penicilin: Penicilin[i],
+            Streptomycin: Streptomycin[i],
+            Neomycin: Neomycin[i],
+            mode: type,
+            type: type,
+            name: data[i]["Bacteria "],
+            x: ['Penicilin', 'Streptomycin', 'Neomycin'],
+            y: Penicilin[i] + Streptomycin[i] +  Neomycin[i]
+        };
+        
+        chartData.push(newChartData); 
+    }
+    var dataArray = [chartData, Penicilin, Streptomycin, Neomycin, nameArray];
+
+    return dataArray;
+}
+
+function drawFirstVis(data, nameArray) {
+     //need three data points (one for each type of antibiotic)
+     //x = array of all names
+     // y = values for each drug 
+     var chartData = [];
+
+     //Penicilin
+     chartData[0] = {
+         mode: "bar",
+         type: "bar",
+         name: "Penicilin",
+         x: nameArray,
+         y: data[0]
+     }
+     
+     //Strepomycin
+     chartData[1] = {
+         mode: "bar",
+         type: "bar",
+         name: "Strepomycin",
+         x: nameArray,
+         y: data[1]
+     }
+     
+     //Neomycin
+     chartData[2] = {
+         mode: "bar",
+         type: "bar",
+         name: "Neomycin",
+         x: nameArray,
+         y: data[2]
+     }
+     
+     var chartLayout = {
+        title: 'Bacteria Visualization 1',
+        xaxis: {
+            title: "Bacteria"
+        },
+        yaxis: {
+        title:   "Penicilin MIC (Minimum Inhibitory Concentration)"
+        },
+        height: 1000,
+        width: window.width,
+        barmode: 'stack'
+    };
     
+    Plotly.plot(document.getElementById('plot1'), chartData, chartLayout, {staticPlot: true});
+}
+
+function drawSecondVis(data) {
+        
     var negGram = {
         y: [],
         label: [],
@@ -35,29 +113,19 @@ function drawFirstVis(data) {
         type: 'box'
     };
     
-    
     for( var i = 0; i < data.length; i++) {
-        //if (data[i]("positive")) {
-        //    console.log('another positive gram stained one!');
-        //}
-        console.log(data[i]);
-        console.log("Penicilin: " + data[i].Penicilin);
-        console.log("Gram Staining: " + data[i]["Gram Staining "]);
         if (data[i]["Gram Staining "] == "positive") {
             posGram.label.push(data[i]["Bacteria "]);
-            
             posGram.y.push(data[i].Penicilin);
-            console.log(posGram);
         } else {
             negGram.label.push(data[i]["Bacteria "]);
             negGram.y.push(data[i].Penicilin);
-            console.log(negGram);
         }
     };
-    
+    var chartData = [negGram, posGram];
 
     var chartLayout = {
-        title: 'Bacteria Visualization v2',
+        title: 'Bacteria Visualization 2',
         xaxis: {
             title: "Gram Positive or Gram Negative"
         },
@@ -68,49 +136,22 @@ function drawFirstVis(data) {
         width: window.width
     };
 
-    var chartData = [negGram, posGram];
-
     Plotly.plot(document.getElementById('plot2'), chartData, chartLayout, {staticPlot: true});     
 }
 
-function drawSecondVis(data) {
-       
-       var Penicilin = [];
-    var Streptomycin = [];
-    var Neomycin = [];
-      
-    var nameArray = [];
+function drawThirdVis(data, nameArray) {
     var chartData = [];
-    
-      for( var i = 0; i < data.length; i++) {
-        //if (data[i]("positive")) {
-        //    console.log('another positive gram stained one!');
-        //}
-        console.log(data[i]);
-        nameArray.push(data[i]["Bacteria "]);
-        console.log("Bacteria: " + data[i]["Bacteria "]);
-        Penicilin.push(data[i].Penicilin);
-        console.log("Penicilin: " + data[i].Penicilin);
-        Streptomycin.push(data[i]["Streptomycin "]);
-        console.log("Streptomycin: " + data[i]["Streptomycin "]);
-        Neomycin.push(data[i].Neomycin);
-        console.log("Neomycin: " + data[i].Neomycin);
-        var newChartData = {
-            Penicilin: data[i].Penicilin,
-            Streptomycin: data[i]["Streptomycin "],
-            Neomycin: data[i].Neomycin,
-            y: [data[i].Penicilin, data[i]["Streptomycin "], data[i].Neomycin],
-            mode: 'lines+markers',
-            name: data[i]["Bacteria "],
-            type: 'scatter'
-        };
+    for (var i = 0; i < data.length; i++) {
+         var newChartData = {
+            mode: "markers+lines",
+            type: "scatter",
+            name: nameArray[i],
+            x: ["Penicilin", "Streptomycin", "Neomycin"],
+            y: [data[i]["Penicilin"], data[i]["Streptomycin"], data[i]["Neomycin"]]
+        }
         chartData.push(newChartData);
-        console.log(newChartData);
-    };
-    console.log("~~~~~~~~~~~~~~~~~~~END OF VARIABLE PREP~~~~~~~~~~~~~~~~~~")
-    console.log(Penicilin);
-    console.log(Streptomycin);
-    console.log(Neomycin);
+    }
+
     var chartLayout = {
         title: 'Bacteria Visualization 3',
         xaxis: {
@@ -120,8 +161,9 @@ function drawSecondVis(data) {
         title:   "MIC (Minimum Inhibitory Concentration)"
         },
         height: 1300,
-        width: window.width
+        width: window.width,
+        type: 'lines+markers'
     };
-
+    
     Plotly.plot(document.getElementById('plot3'), chartData, chartLayout, {staticPlot: true});     
 }
