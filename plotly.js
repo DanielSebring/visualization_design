36a649014ca7a027f6d43d618dbf050af657ffe8
DanielSebring <sebring.d@gmail.com>
2016-04-13 17:21:27 -0700

    comments and final explanation

--- plotly.js
+++ plotly.js
@@ -1,16 +1,27 @@
+/*
+Daniel Sebring
+INFO 474 Visual Design Assignment
 
+This assignment uses a dataset of bacteria and their response to different antibiotics.  
+The assignment is to create and explain three visualizations that might help a user 
+get a better understanding of these datasets.
+*/
 
+//first create this variable so we can populate it with information about the bacteria
 var bacteria_data = [];
-            
+        
+        //We've imported d3 and utilize it so we can read in .csv files    
 d3.csv("data/antibiotics_data.csv", function(error, data) {
-    var bacteriaDataArray = getBacteria(data, "scatter");
-    var bacteria_data = bacteriaDataArray[0];
-    var drugs = [bacteriaDataArray[1], bacteriaDataArray[2], bacteriaDataArray[3]];
-    var nameArray = bacteriaDataArray[4]
+    // initial dataset parsing and setup
+    var bacteriaDataArray = getBacteria(data, "scatter"); //splits data into various attributes
+    var bacteria_data = bacteriaDataArray[0]; //the first array is all objects that represent different bacteria
+    var drugs = [bacteriaDataArray[1], bacteriaDataArray[2], bacteriaDataArray[3]]; //these arrays hold the MIC for the 16 bacteria
+    var nameArray = bacteriaDataArray[4] // the names of all bacteria
 
-    drawFirstVis(drugs, nameArray);
-    drawSecondVis(data);
-    drawThirdVis(bacteria_data, nameArray);
+    // functions to create visualizations
+    drawFirstVis(drugs, nameArray); //draws stacked bar graph
+    drawSecondVis(data); //draws box plot
+    drawThirdVis(bacteria_data, nameArray); //draws scatter plot
 });
 
 function getBacteria(data, type) {
@@ -20,6 +31,8 @@ function getBacteria(data, type) {
     var nameArray = [];
     var chartData = [];
     
+    // parse through all parts of the data set and get their specific values- note, some of the categories in the dataset
+    // end in a whitespace character, so either strip the values or look for which ones end in a " "
     for( var i = 0; i < data.length; i++) {
         //console.log(data[i]);
         nameArray.push(data[i]["Bacteria "]);
@@ -27,6 +40,7 @@ function getBacteria(data, type) {
         Streptomycin.push(data[i]["Streptomycin "]);
         Neomycin.push(data[i].Neomycin);
         
+        //this creates the individual bacteria object, with y being the bacteria's combined MIC
         var newChartData = {
             Penicilin: Penicilin[i],
             Streptomycin: Streptomycin[i],
@@ -45,10 +59,12 @@ function getBacteria(data, type) {
     return dataArray;
 }
 
+
+// In this function we're going to be drawing a stacked bar graph
+// to graph a stacked bar graph in Plotly, we need to give plotly three datasets-
+// datasets for the three stacked categories (the three antibiotics) which should be their MIC for each bacteria as a y value
+// and the x value should be the name of the bacteria
 function drawFirstVis(data, nameArray) {
-     //need three data points (one for each type of antibiotic)
-     //x = array of all names
-     // y = values for each drug 
      var chartData = [];
 
      //Penicilin
@@ -79,7 +95,7 @@ function drawFirstVis(data, nameArray) {
      }
      
      var chartLayout = {
-        title: 'Bacteria Visualization 1',
+        title: 'Total MIC for Different Bacteria',
         xaxis: {
             title: "Bacteria"
         },
@@ -94,6 +110,10 @@ function drawFirstVis(data, nameArray) {
     Plotly.plot(document.getElementById('plot1'), chartData, chartLayout, {staticPlot: true});
 }
 
+
+// In this function we're going to be drawing a box plot 
+// to graph a box plot in Plotly, we're going to need to give Plotly a dataset for each box we
+// want to graph, in this case, all the Gram positive and Gram negative bacteria
 function drawSecondVis(data) {
         
     var negGram = {
@@ -113,6 +133,7 @@ function drawSecondVis(data) {
         type: 'box'
     };
     
+    // if something is gram positive, add it to that dataset, else add it to negative dataset
     for( var i = 0; i < data.length; i++) {
         if (data[i]["Gram Staining "] == "positive") {
             posGram.label.push(data[i]["Bacteria "]);
@@ -125,7 +146,7 @@ function drawSecondVis(data) {
     var chartData = [negGram, posGram];
 
     var chartLayout = {
-        title: 'Bacteria Visualization 2',
+        title: 'MIC of Gram-positive and Gram-negative Bacteria',
         xaxis: {
             title: "Gram Positive or Gram Negative"
         },
@@ -139,6 +160,10 @@ function drawSecondVis(data) {
     Plotly.plot(document.getElementById('plot2'), chartData, chartLayout, {staticPlot: true});     
 }
 
+// In this function we're going to be drawing a connected scatter plot
+// to graph a stacked bar graph in Plotly, we need to give plotly datasets for each bacteria, so we can
+// connect the dots and demonstrate that specific bacteria.  To do that, we set up objects that represent
+// each bactera with x as the array of names of antibiotics and y the array of MICs for that antibiotics/bacteria combo
 function drawThirdVis(data, nameArray) {
     var chartData = [];
     for (var i = 0; i < data.length; i++) {
@@ -153,7 +178,7 @@ function drawThirdVis(data, nameArray) {
     }
 
     var chartLayout = {
-        title: 'Bacteria Visualization 3',
+        title: 'Bacterial Response to Different Antibiotics',
         xaxis: {
             title: "Antibiotic"
         },
